b9f5df1c79deac6214ac40075f68daa568046a77
Gabriel 2000 <166823901+gbr12122000@users.noreply.github.com>
2024-09-30 13:07:33 -0300

    projeto_1_estágio
    sit_gabriel

---
+++ projeto_sit/sit/pages/components/conteudo.php
@@ -0,0 +1,4 @@
+<div class="noticia-container">
+    <img src="https://static.vecteezy.com/ti/fotos-gratis/t2/23628119-uma-preto-e-branco-desenhando-do-uma-pitbull-generativo-ai-foto.jpg" class="noticia-imagem">
+    <a href="" class="noticia-titulo">Título da Notícia</a>
+</div>
---
+++ projeto_sit/sit/pages/politica.php
@@ -0,0 +1,7 @@
+<h1>Política</h1>
+<section align="center">
+    <a href="" class="container-politica">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSu9j7xbb_nVFAL1Rbwa9BuSlmElBPOwhZ5FeRf5IIuP0dvGB54H3Uwkf8soeoORqNkyE&usqp=CAU" alt="" class="img-politica">
+        <p class="text-politica">Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis numquam odit architecto. Similique ad, esse accusantium eius amet veritatis odit aut illo? Necessitatibus hic autem incidunt! Modi ut ab ratione!</p>
+    </a>
+</section>
